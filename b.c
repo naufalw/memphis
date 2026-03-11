6c63153ec006a4ba28aa6176c86bbecf8280c96b
naufalw <naufalwiwitputra@outlook.com>
2026-03-11 20:35:48 +1100

    feat: init GDB wrapper

---
+++ b.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+typedef struct {
+  char letter; /* 1 byte  + 3 bytes padding */
+  int age;     /* 4 bytes */
+  char flag;   /* 1 byte  + 3 bytes padding */
+  float score; /* 4 bytes */
+} Padded;
+
+int main() {
+
+  int counter = 42;
+  char grade = 'A';
+  float pi = 3.14f;
+  char greeting[16] = "Hello, memviz!";
+
+  int scores[5] = {95, 80, 73, 61, 100};
+
+  Padded s = {.letter = 'A', .age = 25, .flag = 1, .score = 9.5f};
+  int *heap_array = malloc(5 * sizeof(int));
+  for (int i = 0; i < 5; i++)
+    heap_array[i] = (i + 1) * 11; /* 11,22,33,44,55 */
+
+  free(heap_array);
+  return 0;
+}
--- main.py
+++ main.py
@@ -1,4 +1,65 @@
 import argparse
+import subprocess
+
+
+class GDB:
+    """
+    GDB wrapper hehe.
+    """
+
+    def __init__(self, binary: str):
+        self.proc = subprocess.Popen(
+            ["gdb", "--interpreter=mi2", "--quiet", binary],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True,
+            bufsize=1,
+        )
+
+    def _readline(self) -> str:
+        "Read output line from gdb"
+        while True:
+            line = self.proc.stdout.readline()
+            if line:
+                return line.strip()
+
+    def _drain(self):
+        "Drain unnecessary yap from gdb"
+        while True:
+            if self._readline().startswith("(gdb)"):
+                return
+
+    def cmd(self, c: str) -> list[str]:
+        """for commands ends with (gdb)"""
+        self.proc.stdin.write(c + "\n")
+        self.proc.stdin.flush()
+        lines = []
+        while True:
+            l = self._readline()
+            if l.startswith("(gdb)"):
+                return lines
+            lines.append(l)
+
+    def run_cmd(self, c: str) -> list[str]:
+        """for commands ends with *stopped"""
+        self.proc.stdin.write(c + "\n")
+        self.proc.stdin.flush()
+        lines = []
+        while True:
+            l = self._readline()
+            lines.append(l)
+            if l.startswith("*stopped"):
+                self._drain()
+                return lines
+
+    def close(self):
+        try:
+            self.proc.stdin.write("-gdb-exit\n")
+            self.proc.stdin.flush()
+        except Exception:
+            pass
+        self.proc.terminate()
 
 
 def main() -> None:
@@ -12,6 +73,19 @@ def main() -> None:
         help="Stdin to feed the program, like '3 10 20 30' ",
     )
     args = p.parse_args()
+
+    gdb = GDB(args.binary)
+
+    loc = str(args.line) if args.line else (args.func or "main")
+
+    gdb.cmd(f"-break-insert {loc}")
+    gdb.run_cmd("-exec-run")
+
+    lines = gdb.cmd("-stack-list-variables --all-values")
+    for l in lines:
+        print(l)
+
+    gdb.close()
 
 
 if __name__ == "__main__":
